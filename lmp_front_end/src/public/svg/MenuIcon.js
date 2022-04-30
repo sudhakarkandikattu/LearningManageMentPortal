e76b7eb893460a90ec265768568f6cee8fa91964
GOUTHAMRAJEN <102846886+GOUTHAMRAJEN@users.noreply.github.com>
2022-04-30 18:48:22 +0530

    Add files via upload

---
+++ lmp_front_end/src/public/svg/MenuIcon.js
@@ -0,0 +1,16 @@
+import React from 'react';
+export const MenuIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24"
+      viewBox="0 0 24 24"
+      width="24"
+      font-size="large"
+      class="fill-current"
+    >
+      <path d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+    </svg>
+  );
+};
